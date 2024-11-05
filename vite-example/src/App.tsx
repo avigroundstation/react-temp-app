ed5002bdd9f8adc190286eb394999f401c911e62
avigroundstation <yehudaroth81@gmail.com>
2024-11-05 11:24:55 +0200

    test git user

--- vite-example/src/App.tsx
+++ vite-example/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />          
         </a>
-        <AviButton> TEst buton </AviButton>
+        <AviButton> test user </AviButton>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
